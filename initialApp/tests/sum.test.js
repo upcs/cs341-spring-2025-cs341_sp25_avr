df82b66984d19a50b1b0fc2f7f9d7b0f2e3d55c8
Emma-N-Jay <emma.nicole.jeppesen@gmail.com>
2025-02-05 13:05:54 -0800

    Added express and some of the HTML. Using Tramanh's version for testing. (So her's is the index.html)

---
+++ initialApp/tests/sum.test.js
@@ -0,0 +1,9 @@
+//Author: Emma Jeppesen (But technically Martin Cenek)
+
+const sum = require('../public/javascripts/sum.js');
+
+test('adds 1 + 2 to equal 3', () => {
+
+expect(sum(1, 2)).toBe(3);
+
+});
