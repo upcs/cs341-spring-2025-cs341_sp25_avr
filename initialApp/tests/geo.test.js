815f375f93e60882ae2e7e574edc190e2b806489
mckinler2 <119765760+mckinler2@users.noreply.github.com>
2025-03-20 09:06:42 -0700

    Update geo.test.js

--- initialApp/tests/geo.test.js
+++ initialApp/tests/geo.test.js
@@ -1,44 +1,48 @@
 const fs = require('fs');
 const { JSDOM } = require('jsdom');
+const { getUserCoords } = require('../public/javascripts/geo');
 
 describe('Geolocation Tests', () => {
-    let document;
-    let mockPosition;
+  let document;
 
-    beforeAll(() => {
-        global.navigator.geolocation = {
-            getCurrentPosition: jest.fn()
-        };
-    });
+  beforeAll(() => {
+    global.navigator.geolocation = {
+      getCurrentPosition: jest.fn(),
+    };
+  });
 
-    beforeEach(() => {
-        const html = fs.readFileSync('public/geo.html', 'utf8');
-        const dom = new JSDOM(html);
-        document = dom.window.document;
-        global.document = document;
-        global.window = dom.window;
+  beforeEach(() => {
+    const html = fs.readFileSync(`${__dirname}/../public/geo.html`, 'utf8');
+    const dom = new JSDOM(html);
+    document = dom.window.document;
+    global.document = document;
+    global.window = dom.window;
 
-        // Create the "details" div if not in HTML
-        const detailsDiv = document.createElement('div');
-        detailsDiv.id = 'details';
-        document.body.appendChild(detailsDiv);
+    const detailsDiv = document.createElement('div');
+    detailsDiv.id = 'details';
+    document.body.appendChild(detailsDiv);
 
-        // Mock geolocation data
-        mockPosition = {
-            coords: {
-                latitude: 45.5725,
-                longitude: -122.7265
-            }
-        };
-    });
+    navigator.geolocation.getCurrentPosition.mockImplementation((success) =>
+      success({
+        coords: {
+          latitude: 45.5725,
+          longitude: -122.7265,
+        },
+      })
+    );
+  });
 
-    it('updates coordinates in HTML', () => {
-        navigator.geolocation.getCurrentPosition.mockImplementationOnce((success) => success(mockPosition));
+  afterEach(() => {
+    document.body.innerHTML = '';
+    jest.clearAllMocks();
+  });
 
-        getUserCoords();
+  it('updates coordinates in HTML', () => {
+    getUserCoords();
 
-        const details = document.getElementById('details');
-        expect(details.innerHTML).toContain('Latitude: 45.5725');
-        expect(details.innerHTML).toContain('Longitude: -122.7265');
-    });
+    const details = document.getElementById('details');
+    expect(details.innerHTML).toContain('Latitude: 45.5725');
+    expect(details.innerHTML).toContain('Longitude: -122.7265');
+  });
 });
+
